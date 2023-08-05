7856775e1b73058ed2000ca63e35aa554c30b27a
nateb <nw3455@gmail.com>
2023-08-05 14:58:51 -0700

    WIP4: Change table to divs

--- src/components/DiffTable.jsx
+++ src/components/DiffTable.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react"
-import { Bar } from "react-chartjs-2"
-import { keys, set } from "d3-collection"
-import { ascending, descending } from "d3-array"
+import React, {useState} from "react"
+import {Bar} from "react-chartjs-2"
+import {keys, set} from "d3-collection"
+import {ascending, descending} from "d3-array"
 
-import { DiffStyled, } from "../utils/utils"
+import {DiffStyled,} from "../utils/utils"
+import Select from "react-select";
 
 const DiffTable = ({data, usePct, years, colors, diffColors}) => {
   const [sortBy, setSortBy] = useState("diff")
@@ -12,7 +13,18 @@ const DiffTable = ({data, usePct, years, colors, diffColors}) => {
     const target = e.target;
     setSortBy(target.value);
   }
+  const options = [
+    {
+      "value": "FY22",
+      "budget_type": "1",
+      "label": "FY22 Adopted",
+      "total": "1196607155"
+    }]
+  const expose = () => {
+  }
 
+  // <option value="diff">amount</option>
+  // <option value="key">name</option>
   const sortFunc = sortBy === "diff" ? descending : ascending
   const allKeys = set()
   keys(data[0]).forEach((key) => {
@@ -66,49 +78,43 @@ const DiffTable = ({data, usePct, years, colors, diffColors}) => {
         ],
       }
       return (
-        <tr key={entry.key}>
-          <td>
+        <div key={entry.key}>
+          <div>
             <h4>
               {entry.key}
               <Bar data={data} height={40}></Bar>
             </h4>
-          </td>
-          <td>
+          </div>
+          <div>
             <DiffStyled
               diff={entry.diff}
               colors={diffColors}
               usePct={usePct}
             ></DiffStyled>
-          </td>
-        </tr>
+          </div>
+        </div>
       );
     });
 
   return (
     // Todo: Use CSS Grid to display 2 rows on xl, 1 row sm, and make both responsive resize
-    <table className="table">
-      <thead>
-        <tr>
-          <th className="">
-            <div className="">
-              <label className="" htmlFor="sortControl">Sort by:{" "}</label>
-              <div className="">
-                <select
-                  className=""
-                  id="sortControl"
-                  value={sortBy}
-                  onChange={updateSort}
-                >
-                  <option value="diff">amount</option>
-                  <option value="key">name</option>
-                </select>
-              </div>
-            </div>
-          </th>
-        </tr>
-      </thead>
-      <tbody>{diffList}</tbody>
-    </table>
+    <div className="table">
+      <div className="">
+        <div className="">
+          <label className="">Sort by:</label>
+          <Select
+            className=""
+            id="sortControl"
+            options={options}
+            value={sortBy}
+            onChange={expose}
+            searchable={false}
+            clearable={false}
+          />
+        </div>
+      </div>
+      <div>{diffList}</div>
+    </div>
   );
 };
 export default DiffTable
