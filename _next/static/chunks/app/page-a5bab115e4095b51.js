f5c6392cf7fa0d909914dfec3c01ef16486ff57e
nateb <nw3455@gmail.com>
2023-08-06 22:11:11 -0700

    Deploy with next build
    Deploy with regular React app instead of Next.js

--- _next/static/chunks/app/page-a5bab115e4095b51.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{6697:function(n,e,u){Promise.resolve().then(u.t.bind(u,576,23))}},function(n){n.O(0,[576,827,759,744],function(){return n(n.s=6697)}),_N_E=n.O()}]);
